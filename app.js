2cd4d2b1f2601be7ba6cbd208451963791b20858
Lauren Cahill <laurencahill@live.com>
2018-09-20 16:24:31 -0400

    progress

--- app.js
+++ app.js
@@ -49,9 +49,6 @@ app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-
-
-
 passport.serializeUser((user, cb) => {
   cb(null, user._id);
 });
@@ -100,7 +97,6 @@ app.use(passport.session());
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
-
 
 const index = require('./routes/index');
 app.use('/', index);
